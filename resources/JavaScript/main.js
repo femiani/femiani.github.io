e697fac509d2dd4d81d2ed312acda4128f20d8cd
femiani <42643363+femiani@users.noreply.github.com>
2018-10-04 22:16:18 +0100

    Add files via upload

---
+++ resources/JavaScript/main.js
@@ -0,0 +1,45 @@
+// var pathname = window. location.pathname; console. log(pathname);
+// var myLink = document.querySelectorAll("a");
+ 
+// function elt(name, attrs, ...children) {
+// let dom = document.createElement(name);
+// for (let attr of Object.keys(attrs)) {
+// dom.setAttribute(attr, attrs[attr]);
+// }
+// for (let child of children) {
+// if (typeof child != "string") dom.appendChild(child);
+// else dom.appendChild(document.createTextNode(child));
+// }
+// return dom;
+// }
+
+// for(let link of myLink){if (link.pathname == pathname){
+//     link.style.backgroundColor=  "rgb(255, 255, 255)";
+//     link.style.color="#000";
+    
+//     console.log(link.pathname)}
+//     else link.addEventListener("click",event=> event.preventDefault());
+
+// }
+// let err1 = elt("span",{id: "err1"},"Invalid mail");
+// let err2 = elt("span",{id:  "err1"},"incorrect password");
+
+// function checkIn(inp){
+// if (inp.value.search == 0){document.body.appendChild(err1);
+
+// }
+// else {console.log("okay");}
+// }
+// let con = document.getElementsByClassName("content");
+// let nav = document.querySelector("nav");
+// let menu = document.getElementById("menu");
+// let off =String(nav.clientWidth + 15);
+//     //nav.style.visibility="visible"
+//     //nav.style.visibility="visible"
+// console.log(menu,off,con[0].Cliewidth);
+
+// menu.addEventListener("click",event=>{
+//     menu.style.visibility="hidden";
+//     nav.style.visibility="visible"
+//    con[0].style.left = off
+// })
